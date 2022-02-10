4879fcb76d02904da18c31792fdfa6ac79befb3e
RandaSaadZobair <Rndasaad1@gmail.com>
2022-02-10 23:01:17 +0200

    3rd

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,40 +17,59 @@ namespace Product_Cart_Shop_Task.Controllers
         public ActionResult Index()
         {
             var Productsslist = Db.ProductDetails.ToList();
+            //var Productsslist=Db.ProductDetails.Find(12);
+            Session.Add("e1", Productsslist);
             return View(Productsslist);
 
         }
 
-        public ActionResult About()
+      
+        public ActionResult GoToCartView(int? Id)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-        public ActionResult GoToCartView(int? ProductId)
-        {
-
-            var GetProduct = Db.ProductDetails.SingleOrDefault(a => a.ProductID == ProductId);
-
+            ProductDetail GetProduct = Db.ProductDetails.SingleOrDefault(a => a.ProductID == Id);
+            int ProductIDD = GetProduct.ProductID;
+            string ProductName = GetProduct.ProductName;
+            int ProductPrice = GetProduct.ProductPrice;
             ProductDetail product = new ProductDetail()
             {
-                ProductName = "Dress",
-                ProductPrice = 50
+                ProductID = ProductIDD,
+                ProductName = ProductName,
+                ProductPrice = ProductPrice
             };
+
             Session["Data"] = product;
             return View();
         }
-        //public ActionResult CalculateTotal()
+
+        public ActionResult CalculateTotal(ProductOrder Order)
+        {
+            int Delievery = 30;
+            ProductDetail GetProduct = Db.ProductDetails.SingleOrDefault(a => a.ProductID == Order.ProductID);
+            int ProductPrice =(GetProduct.ProductPrice != null)?GetProduct.ProductPrice : 0;
+            int Quantity = Order.ProductQuantity;
+            ProductOrder ProductsOrder = new ProductOrder()
+            {
+                ProductID = GetProduct.ProductID,
+                ProductPrice = GetProduct.ProductPrice,
+                ProductQuantity = Order.ProductQuantity
+            };
+            int Calculate = ProductPrice * Quantity + Delievery;
+            Session["ClientOrder"] = ProductsOrder;
+            Session["FinalCheckout"] = Calculate;
+            return View();
+        }
+        //public ActionResult About()
         //{
-        //    var calculate= 30 + 50 * 3;
-        //    return View(calculate);
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
         //}
     }
 
---
+++ Models/ProductOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Product_Cart_Shop_Task.Models
+{
+    public partial class ProductOrder
+    {
+        public int ProductID { get; set; }
+        public int ProductPrice { get; set; }
+        public int ProductQuantity { get; set; }
+    }
+}
